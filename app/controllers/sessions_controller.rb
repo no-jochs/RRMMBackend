e8a586a783a30495798e57855d82e186dab7c8c3
John Ochs <code@johnochs.io>
2023-01-30 04:39:22 -0800

    Back to full Rails app now.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
   def create
-    @user = User.find_by_email(session_params[:email])
-    if @user && @user.authenticate(session_params[:password])
+    @user = User.find_by(email: session_params[:email])
+    if @user&.authenticate(session_params[:password])
       session[:user_id] = @user.id
       render json: @user, status: :ok
     else
