a7a9cc6f0ed74f9f20bcd77a0f5feba1afd24902
John Ochs <code@johnochs.io>
2023-02-05 21:57:37 -0800

    Gets rid of rails asset pipeline files.

--- app/javascript/application.js
+++
@@ -1,5 +0,0 @@
-// Entry point for the build script in your package.json
-//= require @hotwired/turbo-rails
-//= require ./controllers
-//= require * as bootstrap from "bootstrap"
-//= require ./components
